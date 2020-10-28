49190f4430ef15077d85eac25cc446adc7dd8c1f
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-10-28 14:19:24 -0400

    Added dependencies for jackson HTTP

--- tools/build.gradle.kts
+++ tools/build.gradle.kts
@@ -3,4 +3,6 @@ dependencies {
     implementation(project(":engine"))
     implementation(project(":utility"))
     implementation("org.jsoup:jsoup:1.13.1")
+    implementation("commons-io:commons-io:2.8.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.+")
 }
