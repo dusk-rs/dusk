21cb2bd74b7d97560b50d1b422a0e442a20049b5
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 20:09:03 -0500

    Updated Player creation arguments, rights is currently hardcoded.

--- engine/src/main/kotlin/rs/dusk/engine/data/PlayerLoader.kt
+++ engine/src/main/kotlin/rs/dusk/engine/data/PlayerLoader.kt
@@ -1,5 +1,6 @@
 package rs.dusk.engine.data
 
+import com.github.michaelbull.logging.InlineLogger
 import org.koin.dsl.module
 import rs.dusk.engine.client.send
 import rs.dusk.engine.client.ui.InterfaceManager
@@ -8,6 +9,8 @@ import rs.dusk.engine.client.ui.PlayerInterfaceIO
 import rs.dusk.engine.client.ui.detail.InterfaceDetails
 import rs.dusk.engine.entity.character.contain.detail.ContainerDetails
 import rs.dusk.engine.entity.character.player.Player
+import rs.dusk.engine.entity.character.player.PlayerDetails
+import rs.dusk.engine.entity.character.player.social.*
 import rs.dusk.engine.event.EventBus
 import rs.dusk.engine.map.Tile
 import rs.dusk.engine.map.collision.Collisions
@@ -15,6 +18,7 @@ import rs.dusk.engine.path.strat.FollowTargetStrategy
 import rs.dusk.engine.path.strat.RectangleTargetStrategy
 import rs.dusk.network.rs.codec.game.encode.message.SkillLevelMessage
 import rs.dusk.utility.getProperty
+import rs.dusk.utility.inject
 
 /**
  * @author Greg Hibberd <greg@greghibberd.com>
@@ -27,9 +31,29 @@ class PlayerLoader(private val bus: EventBus, private val interfaces: InterfaceD
     private val plane = getProperty("homePlane", 0)
     private val tile = Tile(x, y, plane)
 
+    private val logger = InlineLogger()
+
+    private val namesList: NamesList by inject()
+
+    private val relations: RelationshipManager by inject()
+
+    private val channels: FriendsChatChannels by inject()
 
     fun loadPlayer(name: String): Player {
-        val player = super.load(name) ?: Player(id = -1, tile = tile)
+        val playerNames = namesList.getUserName(name) ?: let {
+            val names = Names(name)
+            namesList.addName(names)
+            logger.info { "Creating new player: $name" }
+            names
+        }
+        //todo: rights
+        val player = super.load(name)
+            ?: Player(id = -1,
+                tile = tile,
+                names = playerNames,
+                details = PlayerDetails(name = playerNames, rights = 2),
+                relations = relations.get(playerNames) ?: relations.create(playerNames)
+            )
         val interfaceIO = PlayerInterfaceIO(player, bus)
         player.interfaces = InterfaceManager(interfaceIO, interfaces, player.gameFrame)
         player.interfaceOptions = InterfaceOptions(player, interfaces, containers)
@@ -39,6 +63,7 @@ class PlayerLoader(private val bus: EventBus, private val interfaces: InterfaceD
         }
         player.interactTarget = RectangleTargetStrategy(collisions, player)
         player.followTarget = FollowTargetStrategy(player)
+        channels.name(player, player.details.name.name)
         return player
     }
 }
