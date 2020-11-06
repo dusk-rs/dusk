478a1ea32827f31ae22cf4190f886c43dc258cea
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 20:09:26 -0500

    Added #updatePrivateStatus ext function.

--- engine/src/main/kotlin/rs/dusk/engine/entity/character/player/chat/PlayerChat.kt
+++ engine/src/main/kotlin/rs/dusk/engine/entity/character/player/chat/PlayerChat.kt
@@ -2,8 +2,20 @@ package rs.dusk.engine.entity.character.player.chat
 
 import rs.dusk.engine.client.send
 import rs.dusk.engine.entity.character.player.Player
+import rs.dusk.engine.entity.character.player.social.ContactsImpl
+import rs.dusk.engine.entity.character.player.social.SocialAffiliations
 import rs.dusk.network.rs.codec.game.encode.message.ChatMessage
+import rs.dusk.utility.inject
 
 fun Player.message(text: String, type: ChatType = ChatType.Game, tile: Int = 0, name: String? = null) {
     send(ChatMessage(type.id, tile, text, name, name?.toLowerCase()?.replace(" ", "_")))
+}
+
+fun Player.updatePrivateStatus(status: ChatFilterStatus)
+{
+
+    val contacts: ContactsImpl by inject()
+
+    contacts.setStatus(this, status)
+
 }
