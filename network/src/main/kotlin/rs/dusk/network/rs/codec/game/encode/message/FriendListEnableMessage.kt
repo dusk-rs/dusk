78338c23f1ef5ad79ffabfebdbdaa358c1826d84
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 19:48:23 -0500

    Added FriendListEnableMessage.kt.

---
+++ network/src/main/kotlin/rs/dusk/network/rs/codec/game/encode/message/FriendListEnableMessage.kt
@@ -0,0 +1,9 @@
+package rs.dusk.network.rs.codec.game.encode.message
+
+import rs.dusk.core.network.model.message.Message
+
+/**
+ * @author Jacob Rhiel <jacob.rhiel@gmail.com>
+ * @created Oct 30, 2020
+ */
+class FriendListEnableMessage : Message
