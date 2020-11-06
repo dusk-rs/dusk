94a582544343ffcbc53b5fbc74654fb135244ea8
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 19:13:17 -0500

    LobbyOnlineStatusMessageDecoder.kt converted to ChatFilterMessageDecoder.kt.

--- network/src/main/kotlin/rs/dusk/network/rs/codec/game/decode/ChatFilterMessageDecoder.kt
+++ network/src/main/kotlin/rs/dusk/network/rs/codec/game/decode/ChatFilterMessageDecoder.kt
@@ -4,12 +4,12 @@ import rs.dusk.core.network.codec.packet.access.PacketReader
 import rs.dusk.core.network.model.packet.PacketMetaData
 import rs.dusk.network.rs.codec.game.GameMessageDecoder
 import rs.dusk.network.rs.codec.game.GameOpcodes.ONLINE_STATUS
-import rs.dusk.network.rs.codec.game.decode.message.LobbyOnlineStatusMessage
+import rs.dusk.network.rs.codec.game.decode.message.ChatFilterUpdateMessage
 
 @PacketMetaData(opcodes = [ONLINE_STATUS], length = 3)
-class LobbyOnlineStatusMessageDecoder : GameMessageDecoder<LobbyOnlineStatusMessage>() {
+class ChatFilterMessageDecoder : GameMessageDecoder<ChatFilterUpdateMessage>() {
 
     override fun decode(packet: PacketReader) =
-        LobbyOnlineStatusMessage(packet.readByte(), packet.readByte(), packet.readByte())
+        ChatFilterUpdateMessage(packet.readByte(), packet.readByte(), packet.readByte())
 
 }
