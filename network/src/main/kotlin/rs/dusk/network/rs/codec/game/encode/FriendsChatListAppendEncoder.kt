9eb9fc9943cbaa3a335fc8fc034716a807c342de
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 20:15:17 -0500

    Added FriendsChatListAppendEncoder.kt which updates the friend list.

---
+++ network/src/main/kotlin/rs/dusk/network/rs/codec/game/encode/FriendsChatListAppendEncoder.kt
@@ -0,0 +1,41 @@
+package rs.dusk.network.rs.codec.game.encode
+
+import rs.dusk.core.network.codec.packet.access.PacketWriter
+import rs.dusk.core.network.model.packet.PacketType
+import rs.dusk.network.rs.codec.game.GameMessageEncoder
+import rs.dusk.network.rs.codec.game.GameOpcodes.FRIEND_LIST_APPEND
+import rs.dusk.network.rs.codec.game.encode.message.FriendsChatListAppend
+
+class FriendsChatListAppendEncoder : GameMessageEncoder<FriendsChatListAppend>() {
+
+    override fun encode(builder: PacketWriter, message: FriendsChatListAppend) {
+        val (worldId, username, displayName, rank) = message
+        builder.apply {
+            writeOpcode(FRIEND_LIST_APPEND, PacketType.BYTE)
+            writeFriendDetails(this, worldId, username, displayName, rank)
+        }
+    }
+
+    companion object
+    {
+
+        fun writeFriendDetails(builder: PacketWriter, worldId: Int, username: String, displayName: String, rank: Int)
+        {
+            builder.apply {
+                writeString(username)
+                val different = username != displayName
+                writeByte(different)
+                if (different) {
+                    writeString(username)
+                }
+                writeShort(worldId)//World
+                writeByte(rank)//Rank
+                if (rank != -128) {
+                    writeString("Dusk $worldId")//Server status
+                }
+            }
+        }
+
+    }
+
+}
