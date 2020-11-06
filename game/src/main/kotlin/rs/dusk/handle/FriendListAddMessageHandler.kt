c7391071823a39f0114fe26afe472fa9629c387a
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 19:48:04 -0500

    Added FriendListAddMessageHandler.kt.

---
+++ game/src/main/kotlin/rs/dusk/handle/FriendListAddMessageHandler.kt
@@ -0,0 +1,48 @@
+package rs.dusk.handle
+
+import io.netty.channel.ChannelHandlerContext
+import rs.dusk.core.network.model.session.getSession
+import rs.dusk.engine.client.Sessions
+import rs.dusk.engine.entity.character.player.chat.message
+import rs.dusk.engine.entity.character.player.social.ContactsImpl
+import rs.dusk.engine.entity.character.player.social.NamesList
+import rs.dusk.network.rs.codec.game.GameMessageHandler
+import rs.dusk.network.rs.codec.game.decode.message.FriendListAddMessage
+import rs.dusk.utility.inject
+
+/**
+ * @author Jacob Rhiel <jacob.rhiel@gmail.com>
+ * @created Oct 30, 2020
+ */
+class FriendListAddMessageHandler : GameMessageHandler<FriendListAddMessage>()
+{
+
+    private val sessions: Sessions by inject()
+
+    private val namesList: NamesList by inject()
+
+    private val contactsImpl: ContactsImpl by inject()
+
+    override fun handle(ctx: ChannelHandlerContext, msg: FriendListAddMessage)
+    {
+
+        val session = ctx.channel().getSession()
+
+        val player = sessions.get(session) ?: return
+
+        val name = namesList.getName(msg.name)
+
+        if(name == null)
+        {
+
+            player.message("Could not find player with the username '${msg.name}'.")
+
+            return
+
+        }
+
+        contactsImpl.addFriend(player, name)
+
+    }
+
+}
