e66d4e9461d41eb841def555892705eeae1a9ffd
JacobRhiel <Jacob.Rhiel@gmail.com>
2020-11-05 19:47:09 -0500

    Added Duration.kt constants.

---
+++ engine/src/main/kotlin/rs/dusk/engine/entity/character/player/social/Duration.kt
@@ -0,0 +1,12 @@
+package rs.dusk.engine.entity.character.player.social
+
+object Duration {
+
+    const val MICROSECOND = 1000L
+    const val MILLISECOND = 1000L * MICROSECOND
+    const val SECOND = 1000L * MILLISECOND
+    const val MINUTE = 60L * SECOND
+    const val HOUR: Long = 60L * MINUTE
+    const val DAY: Long = 24L * HOUR
+
+}
