36d4c0d60f1b0283332d8edf4dd7837727210c41
Greg <greghib@users.noreply.github.com>
2020-12-13 01:04:32 +0000

    Start dump client scripts for world map icons

--- tools/src/main/kotlin/rs/dusk/tools/map/process/ObjectProcessor.kt
+++ tools/src/main/kotlin/rs/dusk/tools/map/process/ObjectProcessor.kt
@@ -1,6 +1,7 @@
 package rs.dusk.tools.map.process
 
 import rs.dusk.cache.Cache
+import rs.dusk.cache.Configs.SCRIPTS
 import rs.dusk.cache.config.decoder.WorldMapInfoDecoder
 import rs.dusk.cache.definition.decoder.ClientScriptDecoder
 import rs.dusk.cache.definition.decoder.ObjectDecoder
@@ -35,14 +36,30 @@ class ObjectProcessor(
             val def = objectDecoder.get(obj.id)
             val mapDefId = def.mapDefinitionId
             if(mapDefId != -1) {
-                var scriptDef = scriptDecoder.getOrNull(mapDefId)
+                var scriptId = getScriptId(mapDefId)
+                var scriptDef = scriptDecoder.getOrNull(scriptId)
                 if (scriptDef == null) {
                     val mapDef = mapInfoDecoder.get(mapDefId)
-                    scriptDef = scriptDecoder.getOrNull(mapDef.clientScript)
+                    scriptId = getScriptId(mapDef.clientScript)
+                    scriptDef = scriptDecoder.getOrNull(scriptId)
                     println(mapDef)
                 }
                 println("$obj ${mapDefId} ${scriptDef}")
             }
         }
+    }
+
+    fun getScriptId(id: Int): Int {
+        val context = 17
+        var scriptId = cache.getArchiveId(SCRIPTS, context or (id shl 10))
+        if(scriptId != -1) {
+            return scriptId
+        }
+        scriptId = cache.getArchiveId(SCRIPTS, (65536 + id shl 10) or context)
+        if(scriptId != -1) {
+            return scriptId
+        }
+        scriptId = cache.getArchiveId(SCRIPTS, context or 0x3fffc00)
+        return scriptId
     }
 }
