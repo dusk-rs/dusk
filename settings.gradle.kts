4edff196afa6a767c4053f1df60a703058e9b256
Tyluur <itstyluur@gmail.com>
2020-12-17 22:10:29 -0500

    encode login response

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
 rootProject.name = "dusk"
-
-include(":cache")
--- src/main/kotlin/rs/dusk/network/codec/login/handle/GameLoginMessageHandler.kt
+++ src/main/kotlin/rs/dusk/network/codec/login/handle/GameLoginMessageHandler.kt
@@ -2,18 +2,11 @@ package rs.dusk.network.codec.login.handle
 
 import inject
 import io.netty.channel.ChannelHandlerContext
-import rs.dusk.core.network.buffer.cipher.isaac.IsaacKeyPair
 import rs.dusk.core.network.channel.setCodec
-import rs.dusk.core.network.message.encode.GenericMessageEncoder
-import rs.dusk.core.network.packet.access.PacketBuilder
-import rs.dusk.core.network.packet.decode.RS2PacketDecoder
-import rs.dusk.core.utility.replace
-import rs.dusk.game.entity.character.player.Player
 import rs.dusk.network.codec.game.GameCodec
-import rs.dusk.network.codec.game.encode.message.MapRegionMessage
 import rs.dusk.network.codec.login.LoginMessageHandler
 import rs.dusk.network.codec.login.decode.message.GameLoginMessage
-import rs.dusk.network.codec.login.encode.message.GameLoginDetails
+import rs.dusk.network.codec.login.encode.message.GameLoginConnectionResponseMessage
 
 /**
  * @author Tyluur <itstyluur@gmail.com>
@@ -25,24 +18,10 @@ class GameLoginMessageHandler : LoginMessageHandler<GameLoginMessage>() {
 	
 	override fun handle(ctx : ChannelHandlerContext, msg : GameLoginMessage) {
 		val pipeline = ctx.pipeline()
-		val keyPair = IsaacKeyPair(msg.isaacKeys)
 		
-		pipeline.writeAndFlush(GameLoginDetails(2, 1, msg.username))
+		pipeline.writeAndFlush(GameLoginConnectionResponseMessage(7))
 		
 		ctx.channel().setCodec(gameCodec)
-		
-		val player = Player(msg.username)
-		val xteas = intArrayOf(
-			154360370,
-			-2039884462,
-			189202037,
-			-1449247101
-		)
-		
-		pipeline.replace("packet.decoder", RS2PacketDecoder(keyPair.inCipher))
-		pipeline.replace("message.encoder", GenericMessageEncoder(PacketBuilder(keyPair.outCipher)))
-		
-		pipeline.writeAndFlush(MapRegionMessage(104, true, player.tile.getChunkX(), player.tile.getChunkY(), xteas))
 	}
 	
 }
