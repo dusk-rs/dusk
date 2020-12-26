b41f878dbcadec80b4a6ee1244d6145ae73e3c08
Tyluur <itstyluur@icloud.com>
2020-12-25 21:05:16 -0500

    add scus juicy stuff

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# kotlin-inline-logger
+
+[![Maven Central](https://img.shields.io/maven-central/v/com.michael-bull.kotlin-inline-logger/kotlin-inline-logger.svg)](https://search.maven.org/search?q=g:com.michael-bull.kotlin-inline-logger) [![CI Status](https://github.com/michaelbull/kotlin-inline-logger/workflows/ci/badge.svg)](https://github.com/michaelbull/kotlin-inline-logger/actions?query=workflow%3Aci) [![License](https://img.shields.io/github/license/michaelbull/kotlin-inline-logger.svg)](https://github.com/michaelbull/kotlin-inline-logger/blob/master/LICENSE)
+
 # Dusk
 
 A highly functioning runescape private server using the #667 client build. 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,132 +1,162 @@
-buildscript {
-	repositories {
-		jcenter()
-	}
-	
-	dependencies {
-		classpath(kotlin("gradle-plugin", version = "1.3.72"))
-	}
-}
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.jetbrains.dokka.gradle.DokkaTask
+
+val ossrhUsername: String? by ext
+val ossrhPassword: String? by ext
+
+description = "A logger facilitating lazily-evaluated log calls via Kotlin's inline classes & functions."
 
 plugins {
-	kotlin("jvm") version "1.3.72"
 	`maven-publish`
-	maven
-	idea
-	java
+	signing
+	kotlin("multiplatform") version "1.3.61"
+	id("org.jetbrains.dokka") version "0.10.1"
+	id("com.github.ben-manes.versions") version "0.27.0"
+	id("net.researchgate.release") version "2.8.1"
 }
 
-val koinVersion = "2.1.5"
-val junitVersion = "5.6.2"
-val exposedVersion = "0.24.1"
-val jacksonVersion = "2.11.0"
-val duskCoreVersion = "1.0.0"
-
-group = "rs.dusk"
-version = "1.0.0"
-
-apply(plugin = "kotlin")
-apply(plugin = "idea")
-apply(plugin = "org.jetbrains.kotlin.jvm")
-
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
-repositories {
-	mavenCentral()
-	mavenLocal()
-	jcenter()
-	maven(url = "https://repo.maven.apache.org/maven2")
-	maven(url = "https://jitpack.io")
-	maven(url = "https://dl.bintray.com/michaelbull/maven")
-}
-
-dependencies {
-	//Main
-	implementation(group = "rs.dusk.core", name = "network", version = duskCoreVersion)
-	implementation(group = "rs.dusk.core", name = "utility", version = duskCoreVersion)
-	implementation(group = "com.github.User", name = "Repo:Tag")
-	
-	implementation(kotlin("stdlib-jdk8"))
-	implementation(kotlin("reflect"))
-	implementation("io.netty:netty-all:4.1.44.Final")
-	implementation(group = "com.displee", name = "rs-cache-library", version = "6.7")
-	implementation(group = "org.yaml", name = "snakeyaml", version = "1.26")
-	implementation(group = "io.github.classgraph", name = "classgraph", version = "4.8.78")
-	implementation(
-		group = "com.michael-bull.kotlin-inline-logger",
-		name = "kotlin-inline-logger-jvm",
-		version = "1.0.2"
-	)
-	implementation(group = "org.koin", name = "koin-core", version = koinVersion)
-	implementation(group = "org.koin", name = "koin-logger-slf4j", version = koinVersion)
-	implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.7")
-	
-	//Logging
-	implementation("org.slf4j:slf4j-api:1.7.30")
-	implementation("ch.qos.logback:logback-classic:1.2.3")
-	
-	//Utilities
-	implementation("com.google.guava:guava:29.0-jre")
-	implementation("org.apache.commons:commons-lang3:3.10")
-	implementation("com.google.code.gson:gson:2.7")
-	implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-	implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
-	implementation("org.postgresql:postgresql:42.2.12")
-	implementation("com.zaxxer:HikariCP:3.4.5")
-	implementation("it.unimi.dsi:fastutil:8.3.1")
-	
-	//Testing
-	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-	testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-	testImplementation(group = "org.koin", name = "koin-test", version = koinVersion)
-	testImplementation(group = "io.mockk", name = "mockk", version = "1.10.0")
-}
-
-
-allprojects {
-	repositories {
-		maven {
-			url("https://jitpack.io")
-			credentials({ username authToken })
+tasks.withType<DependencyUpdatesTask> {
+	rejectVersionIf {
+		listOf("alpha", "beta", "rc", "cr", "m", "eap", "pr").any {
+			candidate.version.contains(it, ignoreCase = true)
 		}
 	}
 }
 
-tasks {
-	compileKotlin {
-		kotlinOptions.jvmTarget = "1.8"
-	}
-	compileTestKotlin {
-		kotlinOptions.jvmTarget = "1.8"
-	}
+val dokka by tasks.existing(DokkaTask::class) {
+	outputFormat = "javadoc"
+	outputDirectory = "$buildDir/docs/javadoc"
 }
 
-
-apply(plugin = "maven-publish")
-val sourcesJar by tasks.registering(Jar::class) {
-	archiveClassifier.set("sources")
-	from(sourceSets.getByName("main").java.srcDirs)
+val javadocJar by tasks.registering(Jar::class) {
+	group = LifecycleBasePlugin.BUILD_GROUP
+	description = "Assembles a jar archive containing the Javadoc API documentation."
+	archiveClassifier.set("javadoc")
+	dependsOn(dokka)
+	from(dokka.get().outputDirectory)
 }
 
-artifacts {
-	archives(sourcesJar.get())
+repositories {
+	mavenCentral()
+	jcenter()
+}
+
+kotlin {
+	sourceSets {
+		named("commonMain") {
+			dependencies {
+				implementation(kotlin("stdlib-common"))
+			}
+		}
+		
+		named("commonTest") {
+			dependencies {
+				implementation(kotlin("test-common"))
+				implementation(kotlin("test-annotations-common"))
+			}
+		}
+	}
+	
+	jvm {
+		compilations.named("main") {
+			kotlinOptions {
+				jvmTarget = "1.8"
+				freeCompilerArgs = listOf("-Xno-call-assertions", "-Xno-receiver-assertions", "-Xno-param-assertions")
+			}
+			
+			dependencies {
+				implementation(kotlin("stdlib-jdk8"))
+				implementation("org.slf4j:slf4j-api:1.7.30")
+			}
+		}
+		
+		compilations.named("test") {
+			kotlinOptions {
+				jvmTarget = "1.8"
+				freeCompilerArgs = listOf("-Xno-call-assertions", "-Xno-receiver-assertions", "-Xno-param-assertions")
+			}
+			
+			dependencies {
+				implementation(kotlin("test"))
+				implementation(kotlin("test-junit"))
+				implementation("org.slf4j:slf4j-jdk14:1.7.30")
+			}
+		}
+		
+		mavenPublication {
+			artifact(javadocJar.get())
+		}
+	}
 }
 
 publishing {
 	repositories {
-		mavenLocal()
+		maven {
+			if (project.version.toString().endsWith("SNAPSHOT")) {
+				setUrl("https://oss.sonatype.org/content/repositories/snapshots")
+			} else {
+				setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+			}
+			
+			credentials {
+				username = ossrhUsername
+				password = ossrhPassword
+			}
+		}
 	}
-	publications {
-		register("mavenJava", MavenPublication::class) {
-			from(components["java"])
-			artifact(sourcesJar.get())
+	
+	publications.withType<MavenPublication> {
+		pom {
+			name.set(project.name)
+			description.set(project.description)
+			url.set("https://github.com/michaelbull/kotlin-inline-logger")
+			inceptionYear.set("2019")
+			
+			licenses {
+				license {
+					name.set("ISC License")
+					url.set("https://opensource.org/licenses/isc-license.txt")
+				}
+			}
+			
+			developers {
+				developer {
+					name.set("Tyluur")
+					url.set("https://www.tyluur.tech")
+				}
+			}
+			
+			scm {
+				connection.set("scm:git:https://github.com/dusk-rs/dusk")
+				developerConnection.set("scm:git:git@github.com:tyluur/dusk.git")
+				url.set("https://github.com/dusk-rs/dusk")
+			}
+			
+			issueManagement {
+				system.set("GitHub")
+				url.set("https://github.com/dusk-rs/dusk/issues")
+			}
+			
+			ciManagement {
+				system.set("GitHub")
+				url.set("https://github.com/dusk-rs/dusk/actions?query=workflow%3Aci")
+			}
+			
+			contributors {
+				contributor {
+					name.set("Greg Hib")
+					url.set("https://github.com/greghib")
+				}
+			}
 		}
 	}
 }
 
-dependencies {
-	implementation(kotlin("script-runtime"))
+signing {
+	useGpgCmd()
+	sign(publishing.publications)
+}
+
+tasks.afterReleaseBuild {
+	dependsOn(tasks.publish)
 }
